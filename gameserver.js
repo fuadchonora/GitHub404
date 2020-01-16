c4d44cbcb911c45c183787b7d82543dfedc66513
Fuad Chonora <fuadchonora@gmail.com>
2020-01-16 16:18:01 +0530

    new modules

--- gameserver.js
+++ gameserver.js
@@ -1,19 +1,15 @@
-//let io = require('./app');
-// const io = require('socket.io')(8088);
-// const redis = require('socket.io-redis');
-// io.adapter(redis({ host: 'localhost', port: 6379 }));
-// const GlobalIoEmitter = require('socket.io-emitter')({ host: '127.0.0.1', port: 6379 });
-
 let express = require('express');
 let httpServer = require("http").Server;
 let app = express();
 let httpserver = httpServer(app);
 let io = require('socket.io')(httpserver);
+
+app.use(express.static(__dirname+'/public'));
+
 app.get('/',(req,res)=>{
     //console.log('user called /')
-    res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname+'/public/index.html');
 })
-
 
 let Server = require('./multiplay/Server');
 
@@ -23,32 +19,6 @@ io.on('connection',function(socket){
     server.onConnected(socket);
 })
 
-// let socket = {key:'a'}
-// server.onConnected(socket);
-// socket = {key:'b'}
-// server.onConnected(socket);
-
-// let playersToJoin = [];
-// let socketsToJion = [];
-// for(let  id in server.PLAYERS){
-//     playersToJoin[id] = server.PLAYERS[id];
-//     socketsToJion[id] = server.SOCKETS[id];
-// }
-// server.createLobby(playersToJoin, socketsToJion);
-
-// for(let lobbyId in server.LOBBIES){
-//     for(let playerId in server.LOBBIES[lobbyId].players){
-//         server.LOBBIES[lobbyId].readyToStart(server.LOBBIES[lobbyId].players[playerId]);
-//         console.log('Player Ready :'+server.LOBBIES[lobbyId].players[playerId].id);
-//     }
-// }
-
-// for(let id in server.PLAYERS){
-//     console.log('Outside Ready status is :'+server.PLAYERS[id].readyStatus);
-// }
-
-//console.log(server.LOBBIES)
-
-httpserver.listen(8788,function(){
+httpserver.listen(5000,function(){
     console.log('server started.');
 });
